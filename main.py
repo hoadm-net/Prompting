bd4617c558bfac0c980d1fff7e6da6cdaf7fe8e4
Hoa Dinh <hoadm.net@gmail.com>
2025-04-26 21:24:51 +0700

    base example

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
 
 
 if __name__ == '__main__':
-    print('Hello, World')
+    load_dotenv()
+
+    model = init_chat_model('gpt-4o-mini', model_provider='openai')
+
+    messages = [
+        SystemMessage('Trong vai trò chuyên gia tư vấn, hãy trả lời giúp tôi một số câu hỏi sau:'),
+        HumanMessage("Xin chào!"),
+    ]
+
+    response = model.invoke(messages)
+    print(response.content)
